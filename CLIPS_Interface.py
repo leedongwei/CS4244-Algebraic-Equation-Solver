c8583e3ab77cb61e3ff66bd00fd0e8444d2f7afb
DongWei <leedongwei@gmail.com>
2016-03-24 17:20:00 +0800

    Fix for Windows vs OS X

--- CLIPS_Interface.py
+++ CLIPS_Interface.py
@@ -1,36 +1,57 @@
+# import os
+from os import read
+import platform
 from subprocess import Popen, PIPE
 from time import sleep
-from fcntl import fcntl, F_GETFL, F_SETFL
-from os import O_NONBLOCK, read
-
-p = Popen('/Users/louis/workspace/CS4244/CLIPS Console', 
-           shell=False, stdin=PIPE, stdout=PIPE, stderr= PIPE)
-
-flags = fcntl(p.stdout, F_GETFL) # get current p.stdout flags
-fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
 
 def write_clips(cmd):
-    p.stdin.write(cmd+'\n')
-    print cmd
-    
+  p.stdin.write(cmd+'\n')
+  # print cmd
+
 def read_clips():
-    try:
-        print read(p.stdout.fileno(),1024)
-    except OSError:
-        pass
+  try:
+    print read(p.stdout.fileno(),1024)
+  except OSError:
+    pass
+
+if platform.system() == 'Darwin':
+  print '\r\n\r\nLoading CLIPS for Mac OS X...'
+  p = Popen('CLIPS_console_mac',
+            shell=False,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr= PIPE)
+
+if platform.system() == 'Windows':
+  print '\r\n\r\nLoading CLIPS for Windows...'
+  p = Popen('CLIPS_console_windows.exe',
+            shell=False,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr= PIPE)
+
+print '\r\n\r\n************************************************************\r\n'
+
+read_clips()
 
 write_clips('(clear)')
 write_clips('(load basic_solver.clp)')
-write_clips('(watch rules)')
-write_clips('(watch facts)')
+
+read_clips()
+# write_clips('(watch rules)')
+# write_clips('(watch facts)')
 # write_clips('(unwatch rules)')
 # write_clips('(unwatch facts)')
 write_clips('(reset)')
 write_clips('(run)')
 
-sleep(0.1)
+
+
+response = raw_input("Enter Equation: (eg. 3 = 2x + 1)\r\n ")
+
+sleep(0.2)
 read_clips()
 
 write_clips('(exit)')
-p.terminate()
-
+# p.terminate()
+Popen.terminate
