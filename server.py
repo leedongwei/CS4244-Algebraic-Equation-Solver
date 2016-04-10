80b1dd0e604f7e2e22a3f263ecc4a942f13d4111
Louis <trthanhquang@gmail.com>
2016-04-10 17:34:54 +0800

    refactor

--- server.py
+++ server.py
@@ -2,125 +2,7 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 app.debug = True
 
-from os import read,path,system
-import platform
-from subprocess import Popen, PIPE, check_output
-from time import sleep
-import sys
-import re
-from interpreter import *
-
-class CLIPS_solver:
-    def __init__(self):
-        self.p = None
-        self.launch_clips()
-
-    def launch_clips(self):
-        if platform.system() == 'Darwin':
-            # print '\r\nLoading CLIPS for Mac OS X...'
-
-            from fcntl import fcntl, F_GETFL, F_SETFL
-            from os import O_NONBLOCK
-
-            #For safety
-            system('killall CLIPS_console_mac')
-            
-            exec_dir = path.dirname(path.realpath(__file__))
-            exec_path = path.join(exec_dir,'CLIPS_console_mac')
-            self.p = Popen(exec_path,
-                      shell=False,
-                      stdin=PIPE,
-                      stdout=PIPE,
-                      stderr= PIPE)
-            flags = fcntl(self.p.stdout, F_GETFL) # get current p.stdout flags
-            fcntl(self.p.stdout, F_SETFL, flags | O_NONBLOCK)
-
-        elif platform.system() == 'Windows':
-            print '\r\nLoading CLIPS for Windows...'
-            self.p = Popen('CLIPS_console_windows.exe',
-                      shell=False,
-                      stdin=PIPE,
-                      stdout=PIPE,
-                      stderr= PIPE)
-        else :
-            print '\r\nOS not supported'
-            return False
-        return True
-    
-    def write_clips(self,cmd):
-        self.p.stdin.write(cmd+'\n')
-
-    def read_clips(self):
-        try:
-            s = read(self.p.stdout.fileno(), 2048)
-            return s
-        except OSError:
-            return None
-
-    def close_clips(self):
-        print 'closing CLIPS'
-        self.p.stdin.write('exit\n')
-        self.p.terminate()
-        Popen.terminate
-    
-    def print_progress(self,log):
-        print 'Equation:'
-        for s in log.splitlines():
-            if s.find('FIRE')!=-1:
-                rule_used = s[s.find('FIRE'):].split()[2]
-                if rule_used!='final_result:':
-                    print "Apply %s "%rule_used
-            if s.find('==>')!=-1 and s.find('equation')!=-1:
-                eq_str = s[s.find('equation')-1:]
-                print '\t',
-                print_equation(eq_str)
-
-    def get_progress_HTML(self,log):
-        output = ''
-        for s in log.splitlines():
-            if s.find('FIRE')!=-1:
-                rule_used = s[s.find('FIRE'):].split()[2]
-                if rule_used!='final_result:':
-                    output += "Apply %s <br>"%rule_used
-            if s.find('==>')!=-1 and s.find('equation')!=-1:
-                eq_str = s[s.find('equation')-1:]
-                output += "$$ %s $$ <br>"%interprete_equation(eq_str)
-        return output
-        
-    def solve(self,equation_str):
-        eqn = ClipsConverter(equation_str)
-        eqn.parse()
-        if eqn.output != 'error':
-            self.write_clips('(clear)')
-            self.write_clips('(load init.clp)')
-            self.write_clips('(deffacts initial_equation (equation %s))'%eqn.output)
-            self.write_clips('(defglobal ?*next_id* = %s)'%(eqn.nextOperatorId))
-            self.write_clips('(load inversion.clp)')
-            self.write_clips('(load first_order_solver.clp)')
-            self.write_clips('(watch rules)')
-            self.write_clips('(watch facts)')
-            self.write_clips('(reset)')
-            self.write_clips('(run)')
-            sleep(0.2)
-
-            try:
-                s= ''
-                tmp = ''
-                while True:
-                    tmp = self.read_clips()
-                    if tmp!=None:
-                        s += tmp                
-                        if s.splitlines()[-1].rstrip()=='CLIPS>':
-                            break
-                    sleep(0.1)
-                print s.replace("CLIPS> ","")
-                self.print_progress(s)
-                return self.get_progress_HTML(s)
-            except KeyboardInterrupt:
-                return None
-        else:
-            print "Please check equation syntax"
-        return None
+from CLIPS_solver import *
 
 solver = CLIPS_solver()
 
@@ -136,6 +18,7 @@ def parse_data():
         return res
     else:
         return "FAILED TO SOLVE"
+    
 if __name__ == "__main__":
     try:
         app.run()
